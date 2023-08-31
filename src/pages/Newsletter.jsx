d9f82623957dfc7b4a7072f70d8972f48cf76e74
Andre <andregozubuyuk@gmail.com>
2023-08-31 22:29:14 +0300

    Updated files

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,5 +1,18 @@
 const Newsletter = () => {
-  return <h1>Newsletter</h1>
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        our newsletter
+      </h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input type="text" name="" className="form-input" />
+      </div>
+    </form>
+  )
 }
 
 export default Newsletter
